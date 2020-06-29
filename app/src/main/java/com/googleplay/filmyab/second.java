3301e7bf7c7ec82ab4dc6b8f531353f24d30262a
haniemeer <haniemeer80@gmail.com>
2020-06-29 10:59:35 -0700

    second activity

--- app/src/main/java/com/googleplay/filmyab/second.java
+++ app/src/main/java/com/googleplay/filmyab/second.java
@@ -2,6 +2,7 @@ package com.googleplay.filmyab;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -12,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
@@ -50,30 +54,41 @@ public class second extends AppCompatActivity {
         textViewover = (TextView) findViewById(R.id.textoverview);
         queue = Volley.newRequestQueue(this);
         String Ur = "https://api.themoviedb.org/3/movie/" + id + "?api_key=980dc71ca56e596b80a66f62f315a4b2";
-        StringRequst request = new StringRequst(Request.Method.GET, Ur, )
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            textViewid.setText(jsonObject.getString("id"));
-            textViewtitle.setText(jsonObject.getString("orginal_title"));
-            textViewover.setText(jsonObject.getString("overview"));
-            Picasso.with(second.this).load("https://api.themoviedb.org/t/p/w600_and_h900_bestv2" + jsonObject.getString());
-            textViewtime.setText(jsonObject.getString("release_date"));
-            textViewlang.setText(jsonObject.getString("orginal_language"));
-            JSONArray array = jsonObject.getJSONArray("production_countries");
-            JSONObject object = array.getJSONObject(0);
-            textViewcount.setText(object.getString("name"));
+        StringRequest stringRequest =new StringRequest(Request.Method.GET, Ur, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
+                try {
+
+                    JSONObject jsonObject = new JSONObject(response);
+                    textViewid.setText(jsonObject.getString("id"));
+                    textViewtitle.setText(jsonObject.getString("orginal_title"));
+                    textViewover.setText(jsonObject.getString("overview"));
+                    Picasso.with(second.this).load("https://api.themoviedb.org/t/p/w600_and_h900_bestv2" + jsonObject.getString("poster_path")).into(imageView);
+                    textViewtime.setText(jsonObject.getString("release_date"));
+                    textViewlang.setText(jsonObject.getString("orginal_language"));
+                    JSONArray array = jsonObject.getJSONArray("production_countries");
+                    JSONObject object = array.getJSONObject(0);
+                    textViewcount.setText(object.getString("name"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error =>", error.toString());
+            }
+        });
         progressBar.setVisibility(View.GONE);
-
-        queue.add(request);
+        queue.add(stringRequest);
     }
 
-    private class StringRequst {
-        public StringRequst(int get, String ur, Object p2) {
-        }
     }
 
-}
+
